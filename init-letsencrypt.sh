767d7ec6c7f335539b3b3eda4983ea6fd93eea53
Hasan Ozkan <hasano@shipmercury.com>
2024-02-21 00:46:29 +0300

    no message

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker compose)" ]; then
   exit 1
 fi
 
-domains=(postgrest.umhabutik.com)
+domains=(api.poqus.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="hsn.ozkan@windowslive.com" # Adding a valid address is strongly recommended
