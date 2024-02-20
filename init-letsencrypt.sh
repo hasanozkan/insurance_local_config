15c62a7a3b6dfbf44dd8f0568e189a529ae8696d
Hasan Ozkan <hasano@shipmercury.com>
2024-02-20 10:14:32 +0300

    no message

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-
-if ! [ -x "$(command -v docker-compose)" ]; then
-  echo 'Error: docker-compose is not installed.' >&2
-  exit 1
-fi
-
-domains=(example.org www.example.org)
+domains=(umhabutik.com www.umhabutik.com)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="" # Adding a valid address is strongly recommended
+email="hsn.ozkan@windowslive.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
